63f77c557e46276e6ec3dc0a7c613790cd54c035
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2022-03-15 12:57:46 +0000

    terminamos de crear el algoritmo de ordenación de tareas

--- Ejercicios/clases/__pycache__/ordenacion_topologica.py
+++ Ejercicios/clases/__pycache__/ordenacion_topologica.py
@@ -1,3 +1,12 @@
 class Tareas:
   def __init__(self, lista_tareas):
     self.lista_tareas = lista_tareas
+  def bubbleSort(self):
+    for i in range(len(self.lista-1)):
+      for j in range(len(self.lista-1)):
+        if self.lista[j] > self.lista[j+1]:
+            self.lista[j], self.lista[j+1] = self.lista[j+1], self.lista[j]
+
+lista= [8,13,5,6,13]
+bubbleSort(lista)
+print(lista)
